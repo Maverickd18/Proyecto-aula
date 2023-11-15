7ffffce13fe4d91169a18424909dbd6e93fa5323
Maverickd18 <maverickdiazserna@gmail.com>
2023-11-14 20:40:26 -0500

    Interfaz sin logica

---
+++ proyecto_deAula/src/PA/P_A.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package PA;
+
+
+
+
+/**
+ *
+ * @author HP
+ */
+public class P_A {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+
+      PA_I gui = new PA_I();
+      gui.setLocationRelativeTo(null);
+      gui.setVisible(true);
+      
+      
+      
+        
+    }
+    
+}
